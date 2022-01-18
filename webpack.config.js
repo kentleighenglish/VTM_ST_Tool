bc3ec1d6c1b9b4b7c8101896d4022604904b152d
Kentleigh English <kentleigh@ikengainnovations.com>
2022-01-18 01:11:30 +0000

    Add webpack-merge to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
+import { merge } from "webpack-merge";
 import path from "path";
 
-const server = {
+const base = {
+	mode: process.env.NODE_ENV !== "development" ? "production" : "development"
+}
+
+const server = merge(base, {
 	entry: "./index.js",
 	output: {
 		"path": path.resolve("./build"),
 		"filename": "server.js"
 	},
-	target: "node",
-	mode: process.env.NODE_ENV !== "development" ? "production" : "development"
-}
+	target: "node"
+})
 
-const client = {
+const client = merge(base, {
 	entry: path.resolve("./src/root"),
 	output: {
 		"path": path.resolve("./public"),
 		"filename": "app.js"
 	},
 	target: "web",
-	mode: process.env.NODE_ENV !== "development" ? "production" : "development"
-}
+	devtool: "hidden-source-map"
+})
 
 export default [server, client];
