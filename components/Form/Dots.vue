e470c655e07542407d77f9f0064eec90bb30bab0
Kentleigh English <kentleigh@ikengainnovations.com>
2022-01-19 18:28:29 +0000

    Add dots display

--- components/Form/Dots.vue
+++ components/Form/Dots.vue
@@ -1,6 +1,9 @@
 <template>
 	<div class="dotsInput">
-		{{ label }}
+		<span class="dotsInput__label">{{ label }}</span>
+		<div class="dots">
+			<div v-for="i in 5" :key="i" class="dots__dot" />
+		</div>
 	</div>
 </template>
 <script>
@@ -11,6 +14,35 @@ export default {
 			type: String,
 			default: null
 		}
-	}
+	},
+	data: () => ({
+		dotCount: 5
+	})
 }
 </script>
+<style lang="scss">
+.dotsInput {
+	display: flex;
+	align-items: center;
+
+	.dotsInput__label {
+		display: flex;
+		width: 100%;
+	}
+
+	.dots {
+		display: flex;
+		width: 100%;
+		max-width: 400px;
+
+		&__dot {
+			width: 12px;
+			height: 12px;
+			margin: 0 4px;
+			border: 1px solid $grey-dark;
+
+			border-radius: 50%;
+		}
+	}
+}
+</style>
