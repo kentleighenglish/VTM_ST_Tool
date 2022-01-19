1788218314c997156d94a88aa0566dffb66b7e5b
Kentleigh English <kentleigh@ikengainnovations.com>
2022-01-19 16:40:22 +0000

    Add Input component type

---
+++ components/Form/Input.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="formInput">
+		Input
+	</div>
+</template>
+<script>
+export default {
+	name: "FormInput",
+	props: {
+		name: {
+			type: String,
+			default: null
+		},
+		label: {
+			type: String,
+			default: null
+		},
+		type: {
+			type: String,
+			default: "text"
+		}
+	}
+}
+</script>
