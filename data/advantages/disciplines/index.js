a6e5ca9767af732afcd3227b0894f4a780069612
Kentleigh English <kentleigh@ikengainnovations.com>
2022-03-25 15:32:55 +0000

    Add disciplines files

---
+++ data/advantages/disciplines/index.js
@@ -0,0 +1,17 @@
+export { default as animalism } from "./animalism";
+export { default as auspex } from "./auspex";
+export { default as celerity } from "./celerity";
+export { default as chimerstry } from "./chimerstry";
+export { default as dementation } from "./dementation";
+export { default as dominate } from "./dominate";
+export { default as fortitude } from "./fortitude";
+export { default as necromancy } from "./necromancy";
+export { default as obfuscate } from "./obfuscate";
+export { default as obtenebration } from "./obtenebration";
+export { default as potence } from "./potence";
+export { default as presence } from "./presence";
+export { default as protean } from "./protean";
+export { default as quietus } from "./quietus";
+export { default as serpentis } from "./serpentis";
+export { default as thaumaturgy } from "./thaumaturgy";
+export { default as vicissitude } from "./vicissitude";
