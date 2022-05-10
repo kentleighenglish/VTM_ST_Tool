5014c697050a40cf76e418a65fb4e86c7b10fa52
Kentleigh English <kentleigh@ikengainnovations.com>
2022-05-10 14:54:28 +0100

    Add mongo

---
+++ server/mongo/index.js
@@ -0,0 +1,7 @@
+import * as sheets from "./sheets";
+
+export { sheets };
+
+export default {
+  sheets,
+};
---
+++ server/mongo/sheets.js
@@ -0,0 +1,57 @@
+import debug from "../debug";
+import { run } from "./_utils";
+
+const COLLECTION = "sheets";
+
+export const createSheet = async (sheet) => {
+	try {
+		const response = await run(
+			(db) =>
+			new Promise((resolve, reject) =>
+			db.collection(COLLECTION).insertOne(
+				sheet,
+				(err, result) => (err ? reject(err) : resolve(result.insertedId))
+			)
+		)
+	);
+
+	if (response) {
+		return response;
+	}
+
+	return null;
+} catch (e) {
+	debug("db:sheets", true)("ERROR", e);
+	return null;
+}
+};
+
+export const updateSheet = async (sheet) => {
+	try {
+		const response = await run((db) =>
+		db.collection(COLLECTION).updateOne({ _id: sheet._id }, { $sheet: set })
+	);
+
+	if (response) {
+		return response;
+	}
+
+	return null;
+} catch (e) {
+	return null;
+}
+};
+
+export const fetchAll = async () => {
+	try {
+		const response = await run(db => db.collection(COLLECTION).find({}).toArray());
+
+		if (response) {
+			return response;
+		}
+
+		return [];
+	} catch (e) {
+		return [];
+	}
+};
