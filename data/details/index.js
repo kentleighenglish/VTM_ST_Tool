49441e5c4cf3db9e1214981f7f476634f2b4bc95
Kentleigh English <kentleigh@ikengainnovations.com>
2022-05-19 01:28:07 +0100

    Add edit restrictions to all edit fields

--- data/details/index.js
+++ data/details/index.js
@@ -26,7 +26,10 @@ export default {
 					type: "text",
 					default: null,
 					meta: {
-						description: "The player who controls this character"
+						description: "The player who controls this character",
+						params: {
+							canEdit: (m, { createMode }) => !createMode
+						}
 					}
 				},
 				chronicle: {
@@ -34,7 +37,10 @@ export default {
 					type: "text",
 					default: null,
 					meta: {
-						description: "The name of your current campaign/story"
+						description: "The name of your current campaign/story",
+						params: {
+							canEdit: (m, { createMode }) => !createMode
+						}
 					}
 				}
 			}
@@ -52,7 +58,10 @@ export default {
 						[key]: archetypes[key].label
 					}), {}),
 					meta: {
-						description: "Nature is the character’s “real” self, the person she truly is. The Archetype a player chooses reflects that character’s deep-rooted feelings about herself, others, and the world. Nature should not be the only aspect of a character’s true personality, merely the most dominant. Nature is also used to determine a character’s ability to regain Willpower points"
+						description: "Nature is the character’s “real” self, the person she truly is. The Archetype a player chooses reflects that character’s deep-rooted feelings about herself, others, and the world. Nature should not be the only aspect of a character’s true personality, merely the most dominant. Nature is also used to determine a character’s ability to regain Willpower points",
+						params: {
+							canEdit: (m, { createMode }) => !createMode
+						}
 					}
 				},
 				demeanor: {
@@ -64,7 +73,10 @@ export default {
 						[key]: archetypes[key].label
 					}), {}),
 					meta: {
-						description: "Demeanor is the way a character presents herself to the outside world. It is the “mask” she wears to protect her inner self. A character’s Demeanor often differs from her Nature, though it might not. Also, Demeanor refers to the attitude a character adopts most often — people change Demeanors as often as they change their minds. Demeanor has no effect on any rules."
+						description: "Demeanor is the way a character presents herself to the outside world. It is the “mask” she wears to protect her inner self. A character’s Demeanor often differs from her Nature, though it might not. Also, Demeanor refers to the attitude a character adopts most often — people change Demeanors as often as they change their minds. Demeanor has no effect on any rules.",
+						params: {
+							canEdit: (m, { createMode }) => !createMode
+						}
 					}
 				},
 				concept: {
@@ -72,7 +84,10 @@ export default {
 					type: "text",
 					default: null,
 					meta: {
-						description: "A character’s concept generally refers to who the character was before becoming a vampire. Many Kindred cling desperately to any salvageable aspects of their former selves — their self-image, their occupation, how they lived, what was unique about them. In their new nocturnal world, echoes of their mortal lives are all that stand between many Kindred and madness. Concept is important because it helps a vampire relate to the world. It’s not a numerical Trait, and it has no mechanical effect on the game. Its benefit is that it allows you to formulate a personality for your character, and it provides an anchor for a vampire who wishes to preserve her dwindling Humanity — or to rail against it. Some sample concepts are presented on p. 80. If you don’t see a concept you like, make one up! Its sole purpose is to help you express your character idea concisely."
+						description: "A character’s concept generally refers to who the character was before becoming a vampire. Many Kindred cling desperately to any salvageable aspects of their former selves — their self-image, their occupation, how they lived, what was unique about them. In their new nocturnal world, echoes of their mortal lives are all that stand between many Kindred and madness. Concept is important because it helps a vampire relate to the world. It’s not a numerical Trait, and it has no mechanical effect on the game. Its benefit is that it allows you to formulate a personality for your character, and it provides an anchor for a vampire who wishes to preserve her dwindling Humanity — or to rail against it. Some sample concepts are presented on p. 80. If you don’t see a concept you like, make one up! Its sole purpose is to help you express your character idea concisely.",
+						params: {
+							canEdit: (m, { createMode }) => !createMode
+						}
 					}
 				}
 			}
@@ -90,7 +105,10 @@ export default {
 						[key]: clans[key].label
 					}), {}),
 					meta: {
-						description: "A character’s Clan is her vampire “family,” the undead legacy into which she was Embraced. Vampires are always of the same Clan as their sires, the vampires who Embrace them. Go back to Chapter Two, look at the templates, and decide which Clan you’d like your character to be. As previously mentioned, the Storyteller may disallow members of certain Clans based on the Sect the chronicle involves. Many chronicles, for example, allow only vampires from the Camarilla Clans, while games run at conventions or by organized play clubs sometimes stipulate specific “venue appropriate” criteria. If a player wishes, she need not choose a Clan at all. Many vampires in the modern nights have blood so diluted that they can truly claim no Clan. Unwanted and scorned, these Clanless “Caitiff” are increasingly common. If you wish to play such a character, simply write “Caitiff” under the Clan heading on the character sheet."
+						description: "A character’s Clan is her vampire “family,” the undead legacy into which she was Embraced. Vampires are always of the same Clan as their sires, the vampires who Embrace them. Go back to Chapter Two, look at the templates, and decide which Clan you’d like your character to be. As previously mentioned, the Storyteller may disallow members of certain Clans based on the Sect the chronicle involves. Many chronicles, for example, allow only vampires from the Camarilla Clans, while games run at conventions or by organized play clubs sometimes stipulate specific “venue appropriate” criteria. If a player wishes, she need not choose a Clan at all. Many vampires in the modern nights have blood so diluted that they can truly claim no Clan. Unwanted and scorned, these Clanless “Caitiff” are increasingly common. If you wish to play such a character, simply write “Caitiff” under the Clan heading on the character sheet.",
+						params: {
+							canEdit: (m, { createMode }) => !createMode
+						}
 					}
 				},
 				generation: {
@@ -98,7 +116,10 @@ export default {
 					type: "number",
 					default: null,
 					meta: {
-						description: "The vampire's generation"
+						description: "The vampire's generation",
+						params: {
+							canEdit: (m, { createMode }) => !createMode
+						}
 					}
 				},
 				sire: {
@@ -106,7 +127,10 @@ export default {
 					type: "text",
 					default: null,
 					meta: {
-						description: "The sire who embraced the kindred"
+						description: "The sire who embraced the kindred",
+						params: {
+							canEdit: (m, { createMode }) => !createMode
+						}
 					}
 				}
 			}
