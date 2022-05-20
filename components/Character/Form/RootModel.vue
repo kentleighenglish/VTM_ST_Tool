b05859ce649acd1e63769d70a225222f28daa394
Kentleigh English <kentleigh@ikengainnovations.com>
2022-05-20 13:59:07 +0100

    Polish reset field button

--- components/Character/Form/RootModel.vue
+++ components/Character/Form/RootModel.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="rootModel">
 		<div v-if="fieldModified" class="rootModel__reset" @click="resetModel()">
-			R
+			<CommonIcon>refresh</CommonIcon>
 		</div>
 		<div class="rootModel__input">
 			<slot />
@@ -33,13 +33,27 @@ export default {
 <style lang="scss">
 .rootModel {
 	position: relative;
+	width: 100%;
 
 	&__reset {
 		position: absolute;
+		z-index: 50;
+
 		right: 100%;
-		margin-right: math.div($gap, 2);
+		margin-right: math.div($gap, 4);
+		margin-top: -1px;
+		padding: math.div($gap, 4);
 
 		color: $grey-light;
+
+		&:hover {
+			color: $primary;
+			cursor: pointer;
+		}
+
+		span {
+			font-size: 1em;
+		}
 	}
 }
 </style>
---
+++ components/Common/Icon.vue
@@ -0,0 +1,27 @@
+<template>
+	<span class="icon">
+		<slot />
+	</span>
+</template>
+<script>
+export default {
+	name: "CommonIcon"
+};
+</script>
+<style lang="scss">
+.icon {
+	font-family: "Material Icons";
+	font-weight: normal;
+	font-style: normal;
+	font-size: 24px;
+	line-height: 1;
+	letter-spacing: normal;
+	text-transform: none;
+	display: inline-block;
+	white-space: nowrap;
+	word-wrap: normal;
+	direction: ltr;
+	-webkit-font-feature-settings: "liga";
+	-webkit-font-smoothing: antialiased;
+}
+</style>
