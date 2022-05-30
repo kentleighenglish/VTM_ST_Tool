117924a64a1b524f91ef1e9daf50018b323a9c5b
Kentleigh English <kentleigh@ikengainnovations.com>
2022-05-30 21:14:20 +0100

    Add lots of physical actions

--- data/actions/physical.js
+++ data/actions/physical.js
@@ -27,3 +27,147 @@ export const dodge = {
 		return rollDice(stats.stamina + stats.athletics);
 	}
 }
+
+export const climbing = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "athletics"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.athletics);
+	}
+}
+
+export const driving = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "drive"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.drive);
+	}
+}
+
+export const drivingQuick = {
+	type: "diceRoll",
+	usedStats: ["wits", "drive"],
+	getOutput: (stats) => {
+		return rollDice(stats.wits + stats.drive);
+	}
+}
+
+export const hunting = {
+	type: "diceRoll",
+	usedStats: ["perception", "survival"],
+	getOutput: (stats) => {
+		return rollDice(stats.perception + stats.survival);
+	}
+}
+
+export const featOfStrength = {
+	type: "diceRoll",
+	usedStats: ["willpower"],
+	getOutput: (stats) => {
+		return rollDice(stats.willpower);
+	}
+}
+
+export const breakingAndEntering = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "larceny"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.larceny);
+	}
+}
+
+export const securityIntrusion = {
+	type: "diceRoll",
+	usedStats: ["intelligence", "larceny"],
+	getOutput: (stats) => {
+		return rollDice(stats.intelligence + stats.technology);
+	}
+}
+
+export const standingJump = {
+	type: "diceRoll",
+	usedStats: ["strength"],
+	getOutput: (stats) => {
+		return rollDice(stats.strength);
+	}
+}
+
+export const runningJump = {
+	type: "diceRoll",
+	usedStats: ["strength", "athletics"],
+	getOutput: (stats) => {
+		return rollDice(stats.strength + stats.athletics);
+	}
+}
+
+export const longRunning = {
+	type: "diceRoll",
+	usedStats: ["stamina", "athletics"],
+	getOutput: (stats) => {
+		return rollDice(stats.stamina + stats.athletics);
+	}
+}
+
+export const forceOpen = {
+	type: "diceRoll",
+	usedStats: ["strength"],
+	getOutput: (stats) => {
+		return rollDice(stats.strength);
+	}
+}
+
+export const pursuit = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "athletics"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.athletics);
+	}
+}
+
+export const vehiclePursuit = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "drive"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.drive);
+	}
+}
+
+export const shadowing = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "stealth"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.stealth);
+	}
+}
+
+export const vehicleShadowing = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "drive"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.drive);
+	}
+}
+
+export const sneaking = {
+	type: "diceRoll",
+	usedStats: ["stamina", "stealth"],
+	getOutput: (stats) => {
+		return rollDice(stats.stamina + stats.stealth);
+	}
+}
+
+export const swimming = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "athletics"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.athletics);
+	}
+}
+
+export const throwing = {
+	type: "diceRoll",
+	usedStats: ["dexterity", "athletics"],
+	getOutput: (stats) => {
+		return rollDice(stats.dexterity + stats.athletics);
+	}
+}
