df6d07ddadcdb7d8f37c22dcd517e629a4a2095e
Kentleigh English <kentleigh@ikengainnovations.com>
2022-06-05 22:38:33 +0100

    Add missing merit

---
+++ data/status/merits/earlyRiser.js
@@ -0,0 +1,15 @@
+
+export const label = "Early Riser";
+
+export const type = "supernatural";
+
+export const cost = 1;
+
+export const relatedStats = [];
+
+export const rollModifier = {
+	difficulty: 0,
+	pool: 0
+}
+
+export const description = "No one can explain it, but you seem to have the ability to work on less rest than your fellow vampires. You always seem to be the first to rise and the last to go to bed even if you’re been out until dawn. Your Humanity or Path score is considered to be 10 for purposes of deciding when you rise each evening. Vampires with this Merit cannot take the Deep Sleeper Flaw.";
