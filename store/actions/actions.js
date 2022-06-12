adb5be37677221370ebe1d8d267be7b6ecfd5122
Kentleigh English <kentleigh@ikengainnovations.com>
2022-06-12 02:43:56 +0100

    Fix trigger action not showing up on server response

--- store/actions/actions.js
+++ store/actions/actions.js
@@ -2,10 +2,10 @@ import {
 	globalPushMessage
 } from "@/store/toast/actions";
 
-export const triggerAction = async ({ commit, dispatch, rootState }, payload) => {
+export const triggerAction = ({ commit, dispatch, rootState }, payload) => {
 	const { socket, events } = rootState.socket;
 
-	await new Promise((resolve) => {
+	return new Promise((resolve) => {
 		socket().emit(events.actions.triggerAction, payload, (error, response) => {
 			if (error) {
 				globalPushMessage(dispatch)({
